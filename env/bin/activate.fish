757679c47a55e407a17501843d8ab4ef8af9de52
Shamim <shamimmaloba@gmail.com>
2022-01-29 10:51:38 +0300

    activated

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/shamim/django/env"
+set -gx VIRTUAL_ENV "/home/shamim/django/src/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
