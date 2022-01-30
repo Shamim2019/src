da0d166a3d669a6249bd6199283ef9966c2efd92
Shamim <shamimmaloba@gmail.com>
2022-01-30 20:28:49 +0300

    final commits

--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'authapp',
+    'authapp.apps.AuthappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
